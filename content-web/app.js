050892b1ab7ececadddd3f0e9c372fff0259664d
tonyma <tony.ma@purple.telstra.com>
2022-05-10 12:23:54 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("fab9fc10-f8c4-4a92-96f4-403a6ec905ca");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
